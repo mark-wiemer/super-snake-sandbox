5d56e25b7bd2e26707ca148f879a66c9797882ec
Mark Wiemer <markwiemer@outlook.com>
2025-01-10 20:52:35 -0800

    Use draw_rect instead of storing child nodes

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,8 +1,4 @@
-extends Node
-
-@export var snake_segment_scene: PackedScene
-# To avoid visual jitter, pre-render the "new tail"
-var backup_segment: ColorRect
+extends Node2D
 
 # game variables
 var score: int
@@ -28,20 +24,31 @@ var direction_queue: Array[Vector2] = [up]
 
 # region Built-in functions
 #* Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	new_game()
 
 #* Called every frame
-func _process(_delta):
+func _process(_delta) -> void:
 	read_input()
 	
-func _input(_event):
+func _input(_event) -> void:
 	if Input.is_action_pressed("quit"):
 		get_tree().quit()
+		
+func _draw() -> void:
+	const color = Color(0.125093, 0.591511, 0, 1)
+	for segment in snake_data:
+		draw_rect(
+			Rect2(
+				((segment * cell_size) + Vector2(0, cell_size)),
+				Vector2(cell_size, cell_size)),
+			color
+		)
+	pass
 # endregion
 
 # region "New game" functions
-func new_game():
+func new_game() -> void:
 	get_tree().paused = false
 	get_tree().call_group("segments", "queue_free") # remove old segments
 	$GameOverMenu.hide()
@@ -53,32 +60,14 @@ func new_game():
 	$Food.position = (food_pos * cell_size) + Vector2(0, cell_size)
 	start_game()
 
-# Make backup_segment point to a new instance.
-# Does not free the old backup_segment!
-func reset_backup_segment():
-	backup_segment = snake_segment_scene.instantiate()
-	backup_segment.hide()
-	add_child(backup_segment)
-
-func generate_snake():
-	snake_segments.clear()
-	reset_backup_segment()
+func generate_snake() -> void:
 	snake_data = SnakeModel.generate_snake(start_pos, -direction_queue[0], 3)
-	for pos in snake_data:
-		add_segment(pos)
-		
-func add_segment(pos):
-	backup_segment.show()
-	var SnakeSegment = backup_segment
-	SnakeSegment.position = (pos * cell_size) + Vector2(0, cell_size)
-	snake_segments.append(SnakeSegment)
-	reset_backup_segment()
 
-func start_game():
+func start_game() -> void:
 	$TickTimer.start()
 # endregion
 	
-func read_input():
+func read_input() -> void:
 	# update movement from keypresses
 	var move_direction = direction_queue.back()
 	if Input.is_action_just_pressed("move_down") and move_direction != up and move_direction != down:
@@ -90,7 +79,7 @@ func read_input():
 	if Input.is_action_just_pressed("move_right") and move_direction != left and move_direction != right:
 		direction_queue.push_back(right)
 
-func _on_tick():
+func _on_tick() -> void:
 	var new_state = SnakeModel.tick(SnakeModel.State.new(
 		cells,
 		direction_queue,
@@ -112,15 +101,12 @@ func _on_tick():
 		$Hud.get_node("ScoreLabel").text = "Score: " + str(score)
 	# currently only supports updating the snake length at most 1 each tick
 	snake_data = new_state.snake
-	if (snake_data.size() > snake_segments.size()):
-		add_segment(snake_data[-1])
-	for i in range(snake_segments.size()):
-		snake_segments[i].position = (snake_data[i] * cell_size) + Vector2(0, cell_size)
+	queue_redraw()
 
-func end_game():
+func end_game() -> void:
 	$GameOverMenu.show()
 	$TickTimer.stop()
 	get_tree().paused = true
 
-func _on_game_over_menu_restart():
+func _on_game_over_menu_restart() -> void:
 	new_game()
